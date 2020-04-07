c7c195daa4e804ac3626b72c71650b9a8e501b82
kin9jon9 <cvj9883@naver.com>
2020-04-07 21:23:08 +0900

    mongoose Model Schema 구체화

--- src/index.ts
+++ src/index.ts
@@ -23,12 +23,12 @@ if (process.env.NODE_ENV === 'prod') {
 
 const app = new Koa();
 const router = new Router();
-const { PORT, SWAGGER_URI } = process.env;
+const { PORT, SWAGGER_URI } = process.env;  
 const MONGO_URI = process.env.MONGO_URI!;
 
 // DB
 mongoose
-  .connect(MONGO_URI, { useNewUrlParser: true, useFindAndModify: false })
+  .connect(MONGO_URI, { useNewUrlParser: true, useFindAndModify: false, useUnifiedTopology: true })
   .then(() => {
     console.log('Connected to MongoDB');
   })
