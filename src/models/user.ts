faae855b662908515bc29f4526838648c75295ff
kin9jon9 <cvj9883@naver.com>
2020-04-11 14:00:40 +0900

    user Model 추가

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,18 +1,45 @@
 import { Schema, model, Document } from 'mongoose';
+export const ObjectId = Schema.Types.ObjectId;
 
 export type UserDocument = Document & {
   userId: String;
-  password: String;
+  nickname: String;
+  profileImageUrl: String;
+  taskIds: [String];
+  token: String;
   joinedDate: Date;
 };
 
 const UserSchema: Schema = new Schema({
-  userId: String,
-  password: String,
-  joinedDate: {
-    type: Date,
-    default: Date.now,
+  userId: {
+    type: String,
+    unique: true,
+    required: true
   },
+  nickname: {
+    type: String,
+    required: true
+  },
+  profileImageUrl: {
+    type: String,
+    required: true
+    //deafult : 디폴트 이미지
+  },
+  taskIds: [
+    {
+      type: ObjectId,
+      required: true,
+      ref: 'Task',
+    }
+  ],
+  token: {
+    type: String,
+    required: true
+  },
+  joinedDate: {
+    type:Date,
+    default:Date.now
+  }
 });
 
 const User = model<UserDocument>('User', UserSchema);
