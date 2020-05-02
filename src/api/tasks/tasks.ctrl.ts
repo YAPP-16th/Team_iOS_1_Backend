74361b04dd14e869c2a4b9a76d902f3f265050d8
SimJunSik <wnstlr24@naver.com>
2020-05-02 17:30:05 +0900

    2020/05/02 회의 내용 기반 수정사항 적용 및 tasks.ctrl.ts에서 쓸데없는 줄바꿈 삭제

--- src/api/tasks/tasks.ctrl.ts
+++ src/api/tasks/tasks.ctrl.ts
@@ -1,16 +1,14 @@
 import { TaskDocument } from './../../models/task';
 import { Context } from 'koa';
 import Task from '../../models/task';
-import User from '../../models/task';
 
 export const isExisted = async (ctx: Context, next: () => void) => {
-
   const { id } = ctx.params;
 
-  try{
+  try {
     const task = await Task.findById(id).exec();
 
-    if(!task){
+    if (!task) {
       ctx.status = 404;
       ctx.body = {
         description: 'Not found task',
@@ -20,7 +18,6 @@ export const isExisted = async (ctx: Context, next: () => void) => {
 
     ctx.state.id = id;
     ctx.state.task = task;
-
   } catch (e) {
     ctx.throw(500, e);
   }
@@ -34,42 +31,41 @@ GET /api/tasks
 export const list = async (ctx: Context) => {
   const user = ctx.state.user;
   const taskIds = user.taskIds;
-  
-  try{
-    const tasks = await Task.find({ _id : {$in : taskIds} }).exec();
+
+  try {
+    const tasks = await Task.find({ _id: { $in: taskIds } }).exec();
     ctx.status = 200;
     ctx.body = {
       description: 'Successed get task list',
-      tasks: tasks 
-    }
+      tasks: tasks,
+    };
   } catch (e) {
     ctx.throw(500, e);
   }
-}
+};
 /* 특정 Task 정보 읽기
 GET /api/tasks/:id
 */
-export const taskInfo = async (ctx : Context) => {
-
+export const taskInfo = async (ctx: Context) => {
   const { task } = ctx.state;
 
-  ctx.status =200;
+  ctx.status = 200;
   ctx.body = {
-    description :'Successed get Task',
-    task : task
-  }
-}
+    description: 'Successed get Task',
+    task: task,
+  };
+};
 
 /* 특정 Task 작성
 POST /api/tasks
 { title, coordinates, tags, memo, iconURL, isFinished, dueDate}
 */
-export const write = async (ctx : Context) => {
+export const write = async (ctx: Context) => {
   const user = ctx.state.user;
-  const task = new Task({ ...ctx.request.body});
+  const task = new Task({ ...ctx.request.body });
 
   //task Document 추가
-  try{
+  try {
     await task.save();
   } catch (e) {
     ctx.throw(500, e);
@@ -79,7 +75,7 @@ export const write = async (ctx : Context) => {
   const taskObjectId = task._id;
   user.taskIds.push(taskObjectId);
 
-  try{
+  try {
     await user.save();
   } catch (e) {
     ctx.throw(500, e);
@@ -87,55 +83,49 @@ export const write = async (ctx : Context) => {
 
   ctx.status = 200;
   ctx.body = {
-    description : 'Successed write Task',
-    user : user
-  }
-
-
-}
+    description: 'Successed write Task',
+    user: user,
+  };
+};
 
 /* 특정 Task 삭제
 DELETE /api/tasks/:id
 */
-export const remove = async (ctx : Context) => {
-  const {user, id} = ctx.state;
+export const remove = async (ctx: Context) => {
+  const { user, id } = ctx.state;
 
-  user.taskIds.pull({_id : id})
+  user.taskIds.pull({ _id: id });
 
-  try{
+  try {
     await user.save();
   } catch (e) {
     ctx.throw(500, e);
   }
 
-  try{
-    await Task.findByIdAndRemove({_id:id});
+  try {
+    await Task.findByIdAndRemove({ _id: id });
   } catch (e) {
     ctx.throw(500, e);
   }
 
   ctx.status = 204;
   ctx.body = {
-    description:"Successed remove Task"
-  }
-
+    description: 'Successed remove Task',
+  };
 };
 
 /* 특정 Task 업데이트
 PATCH /api/tasks/:id
 { 수정할필드1, 수정할필드2, ... }
 */
-export const updateTask = async (ctx : Context) => {
+export const updateTask = async (ctx: Context) => {
+  const { id } = ctx.state;
 
-  const {id} = ctx.state;
-
-  try{
+  try {
     await Task.findByIdAndUpdate(id, ctx.request.body);
   } catch (e) {
     ctx.throw(500, e);
   }
 
   ctx.status = 204;
-}
-
-
+};
