d895e0a6a302f662097a400cfe8f594873efb875
SimJunSik <wnstlr24@naver.com>
2020-05-17 14:38:14 +0900

    tag api 추가에 따른 index.ts들 수정 및 swagger 버전값 변경

--- src/api/index.ts
+++ src/api/index.ts
@@ -2,11 +2,13 @@ import Router from 'koa-router';
 import users from './users';
 import frequents from './frequents';
 import tasks from './tasks';
+import tags from './tags';
 
 const api = new Router();
 
 api.use('/users', users.routes());
 api.use('/frequents', frequents.routes());
 api.use('/tasks', tasks.routes());
+api.use('/tags', tags.routes());
 
 export default api;
